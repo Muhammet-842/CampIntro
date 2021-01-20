c346c09a152b2acf4eeef2f80c740180c4cbbdf4
Muhammet-842 <74255054+Muhammet-842@users.noreply.github.com>
2021-01-20 17:15:39 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri) 
+        {
+            Console.WriteLine("Müşteri Eklendi.Hoşgeldiniz "+ musteri.Adi + " " + musteri.Cinsiyet);
+        }
+        
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi +" "+ musteri.Soyadi +" adlı müşteri silindi.");
+        }
+
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Ad,Soyad,Yaşı: " + musteri.Adi + " " + musteri.Soyadi + " " + musteri.Yas);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri kisi1 = new Musteri();
+            kisi1.Adi = "Hasan Yiğit";
+            kisi1.Soyadi = "Çetin";
+            kisi1.Yas = 18;
+            kisi1.Cinsiyet = "Bey";
+
+            Musteri kisi2 = new Musteri();
+            kisi2.Adi = "Muhammet Ali";
+            kisi2.Soyadi = "Şahal";
+            kisi2.Yas = 18;
+            kisi2.Cinsiyet = "Bey";
+
+            Musteri kisi3 = new Musteri();
+            kisi3.Adi = "Yüksel";
+            kisi3.Soyadi = "Şahal";
+            kisi3.Yas = 52;
+            kisi3.Cinsiyet = "Hanım";
+
+            MusteriManager musterimanager = new MusteriManager();
+
+            Musteri[] musteriler = new Musteri[] {kisi1,kisi2,kisi3 };
+
+            foreach (var kisi in musteriler)
+            {
+                musterimanager.Ekle(kisi);
+                musterimanager.Delete(kisi);
+                musterimanager.List(kisi);
+            }
+
+            
+        }
+    }
+}
