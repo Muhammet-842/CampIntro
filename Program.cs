a50e3a9a0a9712b3039d863b381a5eef63a1c204
Muhammet-842 <74255054+Muhammet-842@users.noreply.github.com>
2021-01-29 13:07:36 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,20 +1,32 @@
-﻿using System;
+﻿//  Oyuncuların sisteme kayıt olabileceği, bilgilerini güncelleyebileceği, kayıtlarını silebileceği bir ortamı simule ediniz.
+// Bilgi doğrulaması yap.Interface
+// Oyun satışı yapılabilecek satış ortamını simule ediniz.Interface
+//  Sisteme yeni kampanya girişi, kampanyanın silinmesi ve güncellenmesi imkanlarını simule ediniz.
+// Satışlarda kampanya entegrasyonunu simule ediniz.Interface
 
-namespace DictHomework
+using System;
+
+namespace GameHomework
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MyDict<string, string> player_Club = new MyDict<string, string>();
+            Game game1 = new Game() {Name= "Erzurum",Price= 50,GameType= "Survival" };
+            Customer customer1 = new Customer() {CustomerID= 1,FirstName="Jale",LastName="De jale",TCNo="31313169690",EMoneyinWallet=true };
+            Campaign campaign = new Campaign() {CampaignName= "Mübarek Cuma",DiscountPrice= 30 };
 
-            player_Club.Add("Mesut Ozil","Fenerbahçe");
-            player_Club.Add("Sosa","Fenerbahçe");
-            player_Club.Add("Drinkwater", "Kasımpaşa");
-            player_Club.Add("Abobakar","Beşiktaş");
-            player_Club.Add("Gustavo","Fenerbahçe");
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.SignIn(customer1);
 
-            player_Club.ShowAll();
+            EnoughManager enoughManager = new EnoughManager();
+            enoughManager.IsEnough(customer1, game1, campaign);
+
+            CampaingManager campaingManager = new CampaingManager();
+            campaingManager.BuywCampaign(game1,customer1,campaign);
+
+
+            
         }
     }
 }
